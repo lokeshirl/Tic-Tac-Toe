190db79558361b5d5139fea81c85e667afd4e9d6
lokeshirl <lokeshkumarjha07+github@gmail.com>
2023-08-01 12:17:11 +0530

    tic tac toe tutorial v0.0.0 is ready ✅

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Board from './Board';
+import './App.css';
+
+const App = () => {
+  console.log('render App');
+  return (
+    <div>
+      <Board></Board>
+    </div>
+  );
+};
+
+export default App;
